1048ff84f1dd1e99b4087ef98f847d52108e707c
bhagi123sew <bhagisew7@gmail.com>
2026-06-18 11:12:11 +0900

    homework7 completed

--- lec07/homework7.py
+++ lec07/homework7.py
@@ -3,44 +3,47 @@ import numpy as np
 def major_chord(f, Fs):
     '''
     Generate a one-half-second major chord, based at frequency f, with sampling frequency Fs.
-
-    @param:
-    f (scalar): frequency of the root tone, in Hertz
-    Fs (scalar): sampling frequency, in samples/second
-
-    @return:
-    x (array): a one-half-second waveform containing the chord
-    
-    A major chord is three notes, played at the same time:
-    (1) The root tone (f)
-    (2) A major third, i.e., four semitones above f
-    (3) A major fifth, i.e., seven semitones above f
     '''
-    raise RuntimeError("You need to write this part")
+    G = np.power(2, 1/12)
+
+    n = np.arange(Fs // 2)
+
+    root = np.cos(2 * np.pi * f * n / Fs)
+    major_third = np.cos(2 * np.pi * (f * G**4) * n / Fs)
+    major_fifth = np.cos(2 * np.pi * (f * G**7) * n / Fs)
+
+    x = root + major_third + major_fifth
+
+    return x
+
 
 def dft_matrix(N):
     '''
     Create a DFT transform matrix, W, of size N.
-    
-    @param:
-    N (scalar): number of columns in the transform matrix
-    
-    @result:
-    W (NxN array): a matrix of dtype='complex' whose (k,n)^th element is:
-           W[k,n] = cos(2*np.pi*k*n/N) - j*sin(2*np.pi*k*n/N)
     '''
-    raise RuntimeError("You need to write this part")
+    W = np.zeros((N, N), dtype=complex)
+
+    for k in range(N):
+        for n in range(N):
+            W[k, n] = np.cos(2*np.pi*k*n/N) - 1j*np.sin(2*np.pi*k*n/N)
+
+    return W
+
 
 def spectral_analysis(x, Fs):
     '''
     Find the three loudest frequencies in x.
-
-    @param:
-    x (array): the waveform
-    Fs (scalar): sampling frequency (samples/second)
-
-    @return:
-    f1, f2, f3: The three loudest frequencies (in Hertz)
-      These should be sorted so f1 < f2 < f3.
     '''
-    raise RuntimeError("You need to write this part")
+    X = np.abs(np.fft.fft(x))
+
+    freqs = []
+
+    for i in range(3):
+        k = np.argmax(X)
+        f = k * Fs / len(x)
+        freqs.append(f)
+        X[k] = 0
+
+    freqs.sort()
+
+    return freqs[0], freqs[1], freqs[2]
